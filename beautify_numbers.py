7838b1dccf4d71aedc1688258a4aa3f08733e233
rndviktor2devman <7irb291wqkmu@mail.ru>
2018-11-11 22:36:53 +0300

    pep8

--- beautify_numbers.py
+++ beautify_numbers.py
@@ -3,12 +3,11 @@ from phonenumbers import carrier, parse, NumberParseException
 from target_db_setup import engine, destination_session
 from sqlalchemy import exc, MetaData, Table, Column, Integer
 
-
 SLEEP_TIME = 10
 metadata = MetaData()
 fixed_orders = Table('fixed_orders', metadata,
-                        Column('contact_phone', Integer),
-                        Column('clean_number', Integer))
+                     Column('contact_phone', Integer),
+                     Column('clean_number', Integer))
 
 
 def convert_to_national(phonenumber, country="RU"):
@@ -30,7 +29,8 @@ def beautify_numbers():
             fixed_orders.c.clean_number.is_(None))
         for order in target_orders:
             clean_number = convert_to_national(order.contact_phone)
-            inserted_number = fixed_orders.update().values(clean_number=clean_number).\
+            inserted_number = fixed_orders.update(). \
+                values(clean_number=clean_number). \
                 where(fixed_orders.c.contact_phone == order.contact_phone)
             connection.execute(inserted_number)
         transaction.commit()
